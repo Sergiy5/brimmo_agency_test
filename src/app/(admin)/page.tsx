0aa93bd9614e8105834b1591b4305130b0337463
Serhii Kushnir <s1983kushnir@gmail.com>
2025-11-26 17:38:57 +0100

    Removed trash? fixed build error

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from 'next';
-import { EcommerceMetrics } from '@/components/forecasting/EcommerceMetrics';
-import StatisticsChart from '@/components/forecasting/StatisticsChart';
 import { Forecasting } from '@/components/forecasting/Forecasting';
 
 export const metadata: Metadata = {
@@ -9,17 +7,9 @@ export const metadata: Metadata = {
 };
 
 export default function Ecommerce() {
-  // grid grid-cols-12
   return (
     <div className="flex flex-col gap-4 md:gap-6">
       <Forecasting />
-      {/* <div className="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics />
-      </div>
-
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div> */}
     </div>
   );
 }
